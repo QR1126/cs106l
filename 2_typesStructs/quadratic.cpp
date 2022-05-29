d16dc1cf90438bfb2821b23f2b2c9f0dcd83e3e9
QR1126 <1193523825@qq.com>
2022-05-29 15:58:55 +0800

    add resources and finish typesStructs

--- 2_typesStructs/quadratic.cpp
+++ 2_typesStructs/quadratic.cpp
@@ -8,10 +8,16 @@ using std::sqrt; using std::endl;
 
 //want: a function names quadratic that will take in 3 doubles and return
 //a combination of: does a solution exist, and if so, what is it?
-{
+std::pair<bool, std::pair<double, double>> quadratic(double a, double b, double c){
 	//get radical, test if negative, return indicating no roots
 	//otherwise get roots and return
-	
+	auto delta = pow(b, 2) - (4.0 * a * c);
+	if (delta < 0) {
+		return std::make_pair(false, std::make_pair(0, 0));
+	}
+	auto root1 = (-1 * b + sqrt(delta)) / (2.0 * a);
+	auto root2 = (-1 * b - sqrt(delta)) / (2.0 * a);
+	return std::make_pair(true, std::make_pair(root1, root2));
 }
 
 int main(){
@@ -24,10 +30,12 @@ int main(){
 	cin >> a >> b >> c;
 	//get roots if they exist
 	//some call to quadratic function!
-
-
-
-	
-
+	// std::pair<bool, std::pair<double, double>> res = quadratic(a, b, c);
+	auto res = quadratic(a, b, c);
+	if (!res.first) {
+		cout << "No solutions exist!" << endl;
+	} else {
+		cout << "Solutions are " << res.second.first << " " << res.second.second << endl;
+	}
 	return 0;
 }
