0da4c3da9fce0eb953437df45317a653b8b25071
QR1126 <1193523825@qq.com>
2022-06-10 10:44:39 +0800

    lecture8 template functions

--- 8_template_functions/count_occurrences.cpp
+++ 8_template_functions/count_occurrences.cpp
@@ -11,17 +11,43 @@
 using std::cout;    using std::endl;
 
 // count_occurrences
+// int count_occurrences(std::vector<std::string> vec, std::string target) {
+//     int res = 0;
+//     for (size_t i = 0; i < vec.size(); i++) {
+//         if (vec[i] == target) res++; 
+//     }
+//     return res;
+// }
+
+// int count_occurrences(std::string str, char target) {
+//     int res = 0;
+//     for (size_t i = 0; i < str.size(); ++i) {
+//         if (str[i] == target) res++;
+//     }
+//     return res;
+// }
+
+template <typename InputIt, typename T>
+int count_occurrences(InputIt begin, InputIt end, T target) {
+    int res = 0;
+    for (InputIt it = begin; it != end; ++it) {
+        if (*it == target) res++;
+    }
+    return res;
+}
 
 int main() {
 
     // vector of strings
     std::vector<std::string> lands = {"Xadia", "Drakewood", "Innean"};
     // how can we count the number of times "Xadia" appears in this?
-    
+    auto res1 = count_occurrences(lands.begin(), lands.end(), "Xadia");
+    cout << res1 << endl;
     // string itself (array of chars)
     std::string str = "Xadia";
     // how can we count the number of times 'a' appears in this?
-
+    auto res2 = count_occurrences(str.begin(), str.end(), 'a');
+    cout << res2 << endl;
     return 0;
 }
 
--- 8_template_functions/template_functions.cpp
+++ 8_template_functions/template_functions.cpp
@@ -7,22 +7,44 @@
 #include <vector>
 #include <iterator>
 #include <utility> // pair
+#include <string.h>
 
 using std::cout;    using std::endl;
 
 // myMin function from lecture
+template <typename T>
+T myMin(T a, T b) {
+    return a < b ? a : b;
+}
+
+template <typename T1, typename T2>
+auto myMin_multiple_type(const T1& a, const T2& b) {
+    return a < b ? a : b;
+}
+
+template <typename T>
+auto myMin_ref(const T& a, const T& b) {
+    return a < b ? *a : *b;
+}
 
 int main() {
 
     /*
      * explicit instantiation of template function (3, 4), (3.2, 3.1), (Frankie, Sathya)
      */
-
+    cout << myMin<int>(3, 4) << endl;
+    cout << myMin<double>(3.2, 3.1) << endl;
+    cout << myMin<std::string>("Frankie", "Sathya") << endl;
 
     /*
      * implicit (type-deduced) instantiation of template function (4, 5), (4, 3.2)
      */
-
+    cout << myMin(4, 5) << endl;
+    cout << myMin_multiple_type(4, 3.2) <<endl;
+    cout << myMin("Frankie", "Sathya") << endl;
+    std::string str1 = "abcd";
+    std::string str2 = "efg";
+    cout << myMin_ref(&str1, &str2) << endl;
     // string literals like the ones below are c-strings, with a "char *" data type.
     // char *'s are pointers, which means myMin is comparing the addresses of these two strings,
     // and not the letters themselves! this is a danger of type deduction.
